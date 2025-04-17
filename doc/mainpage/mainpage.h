f5901105908a95a3b394ae0d1bdfb55cb06b691c
lishifeng <lishifenging@outlook.com>
2025-04-18 00:22:37 +0800

    fix: fix doc errors

--- doc/mainpage/mainpage.h
+++ doc/mainpage/mainpage.h
@@ -1,10 +1,10 @@
 /**
  * @mainpage  LibDriver MAX6675
- * @brief     LibDriver MAX6675 is a full function driver of MAX6675 and it can run in both MCU and Linux platform.
+ * @brief     LibDriver MAX6675 is a full-featured driver of MAX6675 and it can run in both MCU and Linux platform.
  * @version   1.0.0
  * @author    Shifeng Li
  *
- * <h3>history</h3>
+ * <h3>History</h3>
  * <table>
  * <tr><th>Date        <th>Version  <th>Author      <th>Description
  * <tr><td>2022/11/30  <td>1.0      <td>Shifeng Li  <td>first upload
